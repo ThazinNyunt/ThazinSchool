a464b972201ec30001613563f6cf462e579d9493
ThazinNyunt <thazinnyunt06@gmail.com>
2020-05-04 20:20:13 +0630

    update

--- enroll_course.php
+++ enroll_course.php
@@ -13,15 +13,16 @@ $courseId = $_GET['course_id'];
 $userId = $_SESSION['user_id'];
 $enroll_date =date('Y-m-d');
 $search = findEnrollCourse($courseId,$userId);
-$insert = enrollCourse($userId,$courseId,$enroll_date);
+
 ?>
 <div align="center">
     <?php if($search >0):?>
         <h1>You have already enroll this course</h1>
-        <a href="course.php?course_id=<?php echo $courseId; ?>"class="btn btn-primary">Go Back</a>
+        <a href="course.php?id=<?php echo $courseId; ?>"class="btn btn-primary">Go Back</a>
     <?php
         exit();
     endif;
+        $insert = enrollCourse($userId,$courseId,$enroll_date);
         if($insert):?>
         <h1>Enrollment Completed</h1>
         <a href="course.php?id=<?php echo $courseId; ?>"class="btn btn-primary">Start Course</a>
